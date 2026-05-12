7cbc144cbe58132abf316ac5882dbac13d3c0dfe
panda <pandalinoox@gmail.com>
2026-05-12 18:51:25 +0530

    feat: added python venv indicator in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,31 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "dracula",
-                icons_enabled = true,
-            }
-        })
-    end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+
+	config = function()
+		local function venv()
+			local path = os.getenv("VIRTUAL_ENV")
+			return path and (" " .. vim.fn.fnamemodify(path, ":t")) or ""
+		end
+
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+				icons_enabled = true,
+			},
+
+			sections = {
+				lualine_c = {
+					"filename",
+					{
+						venv,
+						cond = function()
+							return vim.bo.filetype == "python"
+						end,
+						color = { fg = "#50FA7B", gui = "bold" },
+					},
+				},
+			},
+		})
+	end,
 }
