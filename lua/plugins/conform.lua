e3829fba531201267c958da82c08d558652ec725
panda <pandalinoox@gmail.com>
2026-05-20 21:11:17 +0530

    update: changed formatter of python from black to ruff

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				python = { "black" },
+				python = { "ruff_fix", "ruff_format" },
 				c = { "clang-format" },
 				cpp = { "clang-format" },
 				java = { "google-java-format" },
