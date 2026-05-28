ec6e375299ebdf2a140bc7cc27835baaeb4e800a
panda <pandalinoox@gmail.com>
2026-05-29 00:58:40 +0530

    feat: add toggle for pytest-lsp

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -170,3 +170,19 @@ map("n", "<leader>xl", toggle_loclist, opts)
 map("n", "<leader>xq", toggle_quickfix, opts)
 
 map("n", "<leader>xt", "<cmd>TodoTelescope<CR>", opts)
+
+-- ======================
+-- Pytest LSP
+-- ======================
+
+vim.api.nvim_create_user_command("PytestLspStart", function()
+	require("lsp.python_test").start_pytest_lsp()
+end, {})
+
+vim.api.nvim_create_user_command("PytestLspStop", function()
+	require("lsp.python_test").stop_pytest_lsp()
+end, {})
+
+vim.api.nvim_create_user_command("PytestLspToggle", function()
+	require("lsp.python_test").toggle_pytest_lsp()
+end, {})
--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -4,14 +4,8 @@ function M.setup(capabilities)
 	vim.lsp.config("pyrefly", {
 		capabilities = capabilities,
 	})
-	vim.lsp.config("pytest_lsp", {
-		cmd = { "pytest-language-server" },
-		filetypes = { "python" },
-		root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "pytest.ini", ".git" },
-	})
 
 	vim.lsp.enable("pyrefly")
-	vim.lsp.enable("pytest_lsp")
 end
 
 return M
