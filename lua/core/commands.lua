ac52d0dcd585dc98d014cca1beed9e0654bb68b7
abhinandancodes <abhinandanghosh82@gmail.com>
2026-07-06 14:57:02 +0530

    feat: add command to check active venv

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -9,3 +9,14 @@ end, {})
 vim.api.nvim_create_user_command("PytestLspToggle", function()
 	require("lsp.python_test").toggle_pytest_lsp()
 end, {})
+
+vim.api.nvim_create_user_command("Venv", function()
+	local path = os.getenv("VIRTUAL_ENV")
+	if path then
+		local venv_name = vim.fn.fnamemodify(path, ":t")
+		vim.notify("󱔎 Active Venv: " .. venv_name, vim.log.levels.INFO)
+	else
+		vim.notify("No active virtual environment found", vim.log.levels.WARN)
+	end
+end, {})
+
