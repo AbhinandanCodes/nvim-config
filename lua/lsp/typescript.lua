100610bae3b02b8aa8452c4a2d09c2efcd0c1eb9
abhinandancodes <abhinandanghosh82@gmail.com>
2026-07-14 01:08:39 +0530

    refactor: ts_ls support global typescript

--- lua/lsp/typescript.lua
+++ lua/lsp/typescript.lua
@@ -1,9 +1,20 @@
 local M = {}
 
 function M.setup(capabilities)
-  vim.lsp.config("ts_ls", {
-    capabilities = capabilities,
-  })
+	local npm_root = vim.fn.system("npm root -g"):gsub("%s+", "")
+	local global_ts_path = npm_root .. "/typescript/lib"
+
+	vim.lsp.config("ts_ls", {
+		capabilities = capabilities,
+		init_options = {
+			tsserver = {
+				path = global_ts_path,
+			},
+		},
+	})
+
+	vim.lsp.enable("ts_ls")
 end
 
 return M
+
